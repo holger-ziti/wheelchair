3909b944c2507a321b9cb79a8c2319c4573c8edb
holger-ziti <holger.dieterich@ziti.uni-heidelberg.de>
2021-10-28 13:12:55 +0200

    splitted launch files for different distributions (foxy and eloquent)

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
 # wheelchair
 code related to the wheelchair research platform 
 
-
-### create symbolic link
-
+### ssh into BBB
 ```console
-ln -s ~/git/wheelchair/ros2_packages/force_joystick ~/dev_ws/src
+ssh debian@b3-aut11
+cd ~/git
+```
+
+### first time: get code
+```console
+git clone https://github.com/holger-ziti/wheelchair.git
+ln -s ~/git/wheelchair/ros2_packages/force_joystick ~/ws/src
+```
+
+### update code
+```console
+cd ~/git/wheelchair
+git pull
+```
+
+### source
+```console
+source /home/debian/ros2_eloquent/setup.bash
+source /home/debian/ros2_eloquent/local_setup.bash
+source /home/debian/ws/install/local_setup.bash
+source /home/debian/ws/install/setup.bash
+export ROS_DOMAIN_ID=100
+```
+
+### build code
+```console
+cd ~/ws
+colcon build
+```
+
+### run code
+```console
+ros2 launch force_joystick two_analog_in.launch.py
 ```
 
 
---
+++ ros2_packages/force_joystick/launch/two_analog_in_eloquent.launch.py
@@ -0,0 +1,41 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    node1 = Node(
+            package='force_joystick',
+            node_executable='voltage',
+            node_name='test1',
+            parameters=[
+                {'analog_input_number': 0}
+            ],
+            output='screen',
+            emulate_tty=True
+        )
+    node2 = Node(
+            package='force_joystick',
+            node_executable='voltage',
+            node_name='test2',
+            parameters=[
+                {'analog_input_number': 1}
+            ],
+            output='screen',
+            emulate_tty=True
+        )
+    node3 = Node(
+            package='force_joystick',
+            node_executable='force_cmd.py',
+            node_name='test3',
+            parameters=[
+                {'raw_voltage_number_1': 0},
+                {'raw_voltage_number_2': 1}
+            ],
+            output='screen',
+            emulate_tty=True
+        )
+    ld.add_action(node1)
+    ld.add_action(node2)
+    ld.add_action(node3)
+    return ld
