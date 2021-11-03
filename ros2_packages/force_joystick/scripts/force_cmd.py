2c90fe6b5c32ae0f82c8911781bb4cd9cef8450a
holger-ziti <holger.dieterich@ziti.uni-heidelberg.de>
2021-11-03 13:10:54 +0100

    more damping

--- ros2_packages/force_joystick/scripts/force_cmd.py
+++ ros2_packages/force_joystick/scripts/force_cmd.py
@@ -76,8 +76,8 @@ class CommandVelocityFromForcesPublisher(Node):
         # todo: integration is drifting, use threshold?
 
         a = 0.75 # low-pass filter for force/voltage value
-        b1 = 0.8 # damping parameter
-        b2 = 10.0
+        b1 = 50.0 # damping parameter
+        b2 = 50.0
 
         self.force_1 = a * self.force_1 + (1-a) * (self.voltage_int1 - self.mean_1)
         damping_force_1 = - self.cmd.linear.x * b1
